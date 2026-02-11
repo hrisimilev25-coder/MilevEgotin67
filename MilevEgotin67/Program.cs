70afcfb357fe92a08cd1363fc513f077ed88d948
adnanbts <varban.vatralov.pp25@pgmtplovdiv.info>
2026-02-11 10:06:33 +0200

    Add project files.

---
+++ MilevEgotin67/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MilevEgotin67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Въведете десетично число: ");
+            int n = int.Parse(Console.ReadLine());
+            string binary = "";
+            while (n > 0)
+            {
+                binary = (n % 2) + binary;
+                n /= 2;
+            }
+            Console.WriteLine($"Двоично: Камбуракис ала КМК {binary}");
+        }
+    }
+}
